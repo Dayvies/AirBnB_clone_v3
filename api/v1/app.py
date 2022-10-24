73dc5f80c6943ffe3514e05e689af2476afcb3c0
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-10-24 16:17:26 +0300

    CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,10 +3,12 @@
 from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 host = "0.0.0.0"
 port = 5000
